6c5543a050f10f0fd32abb21079b0f679f729889
ShanTou-U <dev@ShanTou.University>
2024-11-03 10:48:56 +0800

    更新 rss 配置

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
     title: 'ST.U Space',
     description: 'ST.U 的试验田。',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.{md,markdown,mdown,mkd,mdwn,mdx}')),
     customData: `<language>zh-cn</language>`,
   });
 }
